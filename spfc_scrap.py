343e0586a37f569c76e4d7fb512b36b221998445
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2021-12-29 12:58:21 -0300

    Update spfc_scrap.py

--- spfc_scrap.py
+++ spfc_scrap.py
@@ -1,8 +1,33 @@
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import pandas as pd
+from datetime import datetime
 
-query_selector = "#page > div.fake_container > div.page > table:nth-child(11)"
+# query_selector = "table:nth-child(11)"
+
+
+def dataframe_spfc(inicio: int, nowyear: int = None, id_comp=51):
+    """
+    :param inicio: O ano mais antigo p/ iniciar a extrair da competação
+    :param nowyear: Até que ano deseja ver os dados?
+    :param id_comp: cod competição
+
+    Cria dataframe com os anos selecionados...
+
+    """
+    if nowyear is None:
+        nowyear = datetime.now().year
+
+    comp_oldest = nowyear - inicio
+    if comp_oldest > 50:
+        raise ValueError(f"{nowyear} - {inicio} = {comp_oldest}")
+
+    for i in range(1, comp_oldest):
+        pass
+
+
+input(dataframe_spfc(1980))
+
 res = requests.get(
     'https://www.ogol.com.br/equipa_competicao.php?id_comp=51&id_epoca=0&op=&id_equipa=2256&id_jogo=0')
 # page=1
